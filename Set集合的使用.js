d1af847554841c302f741ee4aee3f7ea00b3ee2e
my <my@12345qq.com>
2019-06-16 11:09:35 +0800

    增加Map类的使用

--- Set集合的使用.js
+++ Set集合的使用.js
@@ -33,3 +33,59 @@ let set = new Set();
   7、keys  //获取集合的键（但是其实是获取的是值。因为集合没有键）（返回值是一个迭代器）
   8、size  //获取集合的大小
   9、values //获取集合的所有值（返回值是一个迭代器）
+
+
+
+/*
+  关于对象属性名的使用
+*/
+
+在ES6后，增加了对对象字面量的属性进行动态赋值，但是动态赋值如果不是字符串，他会自动调用
+toString()方法或者String()实例化成字符串
+
+let a = {}
+
+let obj = {[a]:a}  //会调用{}的toString()方法
+
+//为什么要这么做呢？
+  因为，对象的属性是要经过hash的，而hash收拾不可变的，因此，如果是其他可变类型，那么就
+  不满足hash条件，并且，属性名作为对象中的唯一标识，如果重复则会覆盖
+
+举例：
+  var a = {}
+  var obj = {[a]:a}
+  console.log(obj)  //{[object Object]: {…}}
+
+  var a = []
+  var obj = {[a]:a}  //{"": Array(0)}
+
+  var a = null
+  var obj = {[a]:a}
+  console.log(obj)    //{null: null}
+
+  var a = undefined
+  var obj = {[a]:a}
+  console.log(obj)    //{undefined: undefined}
+
+
+/*
+  那么有没有一种直接让一些变量成为键呢？
+  答案是：有的
+  比如：Map
+
+  let map = new Map()
+*/
+
+let map = new Map()
+
+Map实例的方法和属性：
+  map.clear()  //清除所有的键值对
+  map.delete()      //删除指定的键值对
+  map.entries()   //获取键值对的组成的数组的迭代器
+  map.forEach()   //遍历map中的每一个键值对
+  mao.get(key)  //得到指定的键对应的值
+  map.has(key)  //判断指定的键是否存在
+  map.keys()   //获取到map中的所有键
+  map.size   //获取map中的键值对个数
+  map.values()  //获取到map中的所有值
+  map.set(key,value)  //设置一个键值存储在map中
